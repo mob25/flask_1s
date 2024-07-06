8d1bf3bdc369190f15dc728dce00b928a19afc33
Alexander <mob25@bk.ru>
2024-07-06 14:50:16 +0300

    Проверено

--- app.py
+++ app.py
@@ -10,27 +10,48 @@ def index():
 
 @app.route('/jeans')
 def jeans():
-    return render_template("jeans.html")
+    jeans_list = [
+        {'model': 'Джинсы PAGALEE DENIM', 'size':43, 'price':1500},
+        {'model': 'Джинсы Brostem', 'size':42, 'price':1600},
+        {'model': 'Джинсы Omaqlo', 'size':41, 'price':1700}
+    ]
+    return render_template("jeans.html", jeans_list = jeans_list, title = 'Джинсы')
 
 
 @app.route('/shoes')
 def shoes():
-    return render_template("shoes.html")
+    shoes_list = [
+        {'model': 'Кеды Bi Bari', 'size':43, 'price':1500},
+        {'model': 'Туфли GUSTAS', 'size':42, 'price':1600},
+        {'model': 'Туфли ZOLINBERG', 'size':41, 'price':1700}]
+    return render_template("shoes.html", shoes_list = shoes_list, title = 'Обувь' )
 
 
 @app.route('/jackets')
 def jackets():
-    return render_template("jackets.html")
+    jackets_list = [
+        {'model': 'Куртка KITT LANGZHI', 'size':43, 'price':1500},
+        {'model': 'Куртка джинсовая S-GEAR', 'size':42, 'price':1600},
+        {'model': 'Куртка джинсовая iUkoworld', 'size':41, 'price':1700}]
+    return render_template("jackets.html", jackets_list = jackets_list, title = 'Куртки')
 
 
 @app.route('/baasploa')
 def baasploa():
-    return render_template("baasploa.html")
+    title = 'Кроссовки BAASPLOA'
+    product =  {'description': 'Легкие, удобные и яркие - эти женские спортивные \
+    кроссовки BAASPLOA на шнуровке идеальны для весны, лета и демисезона. Они стали \
+    незаменимыми сникерами для активного отдыха и тренировок.',
+    'img': '/static/images/baasploa.jpg',
+    'price': 700}
+    return render_template("baasploa.html", title = title, product = product)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    title = 'Страница не найдена'
+    text = 'Такой страницы не существует'
+    return render_template('404.html', title = title, text = text), 404
 
 
 if __name__ == '__main__':
